f4fe3a7068a0e38e8c0d472d47060a5ddf879b36
cwyang33 <roger.yang33@gmail.com>
2024-11-20 23:08:22 +0800

    update 1

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const line = require('@line/bot-sdk');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 // create LINE SDK config from env variables
 const config = {
@@ -29,18 +35,31 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-function handleEvent(event) {
+async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  console.log("handleEvent $$$ gpt-3.5");
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{
+      role: 'user',
+      content: event.message.text,
+    }],
+    max_tokens: 200,
+  });
+  
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: choices.message.content.trim() || '抱歉，我沒有話可說了。' };
+
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
+
 
 // listen on port
 const port = process.env.PORT || 3000;
